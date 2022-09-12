f14858767644e3d10283a3f0d3002460db1c2468
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-09-11 21:26:58 -0700

    Create toggle functions
    Create the functions that will show and hide the three main screens
    of the game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
+import { toggleMenuScreen, toggleGameScreen, toggleOverlayScreen } from "./toggleScreens.js";
+
 /**
  * Listens for click on `.mark-selection`,
  * Add selected class to current target,
  * Remove the selected class from its sibling
  */
-const markSelection = document.querySelector(".mark-selection");
-markSelection.addEventListener("click", function (event) {
+document.querySelector(".mark-selection").addEventListener("click", function (event) {
     if (event.target.classList.contains("mark-selection__x")) {
-        event.target.classList.add("selected");
-        event.target.nextElementSibling.classList.remove("selected");
-    } else {
-        event.target.classList.add("selected");
-        event.target.previousElementSibling.classList.remove("selected");
+        event.target.classList.toggle("selected");
+        event.target.nextElementSibling.classList.toggle("selected");
     }
+    if (event.target.classList.contains("mark-selection__o")) {
+        event.target.classList.toggle("selected");
+        event.target.previousElementSibling.classList.toggle("selected");
+    }
+});
+
+document.querySelector("#new-game-buttons").addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn-new-game--cpu")) {
+        console.log("CPU");
+        toggleMenuScreen();
+        toggleGameScreen();
+    }
+    if (event.target.classList.contains("btn-new-game--player")) {
+        console.log("PLAYER");
+        toggleMenuScreen();
+        toggleGameScreen();
+    }
+});
+
+document.querySelector(".btn-restart").addEventListener("click", function () {
+    toggleOverlayScreen();
 });
---
+++ js/toggleScreens.js
@@ -0,0 +1,15 @@
+const game = document.querySelector(".game");
+const menu = document.querySelector(".new-game");
+const overlay = document.querySelector(".overlay");
+
+export function toggleMenuScreen() {
+    menu.style.display === "none" ? (menu.style.display = "block") : (menu.style.display = "none");
+}
+
+export function toggleGameScreen() {
+    game.style.display === "none" ? (game.style.display = "block") : (game.style.display = "none");
+}
+
+export function toggleOverlayScreen() {
+    overlay.style.display === "none" ? (overlay.style.display = "block") : (overlay.style.display = "none");
+}
